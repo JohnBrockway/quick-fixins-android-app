f3f6080456e2f015b2c8fd41b4fefbd411efeb65
JohnBrockway <john.c.brockway@gmail.com>
2018-07-27 13:45:48 -0400

    Initial commit

---
+++ app/src/main/java/com/ethosgames/quickfixins/SplashPageActivity.java
@@ -0,0 +1,13 @@
+package com.ethosgames.quickfixins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+    }
+}
