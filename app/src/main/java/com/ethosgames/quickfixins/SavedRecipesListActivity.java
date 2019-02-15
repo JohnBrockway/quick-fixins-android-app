95756c1f65197ddf18bc0a55e30fc582d527a805
JohnBrockway <john.c.brockway@gmail.com>
2019-02-15 16:52:17 -0500

    Add loading page to Saved Recipe List activity

--- app/src/main/java/com/ethosgames/quickfixins/SavedRecipesListActivity.java
+++ app/src/main/java/com/ethosgames/quickfixins/SavedRecipesListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -28,6 +29,8 @@ public class SavedRecipesListActivity extends BaseToolbarActivity {
 
     private final ArrayList<Recipe> recipes = new ArrayList<>();
 
+    private View[] layouts = new View[3];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,49 +39,57 @@ public class SavedRecipesListActivity extends BaseToolbarActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.saved_recipe_list_activity_title);
 
+        layouts[0] = findViewById(R.id.loadingSavedRecipesLayout);
+        layouts[1] = findViewById(R.id.noSavedRecipesLayout);
+        layouts[2] = findViewById(R.id.recipeListLayout);
+
         savedRecipeIds = FileInteractor.readIntegerSetFromFile(getString(R.string.saved_recipes_file_path), getApplicationContext());
-
-        recipesLayoutManager = new LinearLayoutManager(this);
-
-        // TODO add loading indicator to layout, switch to actual layout on response
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String baseUrl = String.format("%s%s?",
-                getApplicationContext().getString(R.string.backend_base_url),
-                getApplicationContext().getString(R.string.backend_get_multiple_by_id_path));
-
-        for (Integer i : savedRecipeIds) {
-            baseUrl += "ids[]=" + i.toString() + "&";
+        if (savedRecipeIds.size() == 0) {
+            setSingleLayoutVisible(R.id.noSavedRecipesLayout);
         }
+        else {
+            recipesLayoutManager = new LinearLayoutManager(this);
 
-        // Remove the last '&' character added above
-        if (savedRecipeIds.size() > 0) {
-            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
-        }
+            // TODO add loading indicator to layout, switch to actual layout on response
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String baseUrl = String.format("%s%s?",
+                    getApplicationContext().getString(R.string.backend_base_url),
+                    getApplicationContext().getString(R.string.backend_get_multiple_by_id_path));
 
-        JsonArrayRequest getRecipesRequest = new JsonArrayRequest(baseUrl,
-                new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                try {
-                    for (int i = 0 ; i < response.length() ; i++) {
-                        recipes.add(new Recipe((JSONObject) response.get(i)));
-                    }
-
-                    recipesRecyclerView = findViewById(R.id.savedRecipes);
-                    recipesRecyclerView.setHasFixedSize(true);
-                    recipesRecyclerView.setLayoutManager(recipesLayoutManager);
-                    recipesAdapter = new SavedRecipesAdapter(recipes, savedRecipeIds);
-                    recipesRecyclerView.setAdapter(recipesAdapter);
-                } catch (Exception e ) {
-                    e.printStackTrace();
-                }
+            for (Integer i : savedRecipeIds) {
+                baseUrl += "ids[]=" + i.toString() + "&";
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {}
-        });
 
-        queue.add(getRecipesRequest);
+            // Remove the last '&' character added above
+            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
+
+            JsonArrayRequest getRecipesRequest = new JsonArrayRequest(baseUrl,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            try {
+                                for (int i = 0; i < response.length(); i++) {
+                                    recipes.add(new Recipe((JSONObject) response.get(i)));
+                                }
+
+                                recipesRecyclerView = findViewById(R.id.savedRecipes);
+                                recipesRecyclerView.setHasFixedSize(true);
+                                recipesRecyclerView.setLayoutManager(recipesLayoutManager);
+                                recipesAdapter = new SavedRecipesAdapter(recipes, savedRecipeIds);
+                                recipesRecyclerView.setAdapter(recipesAdapter);
+
+                                setSingleLayoutVisible(R.id.recipeListLayout);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {}
+            });
+
+            queue.add(getRecipesRequest);
+        }
     }
 
     @Override
@@ -118,5 +129,16 @@ public class SavedRecipesListActivity extends BaseToolbarActivity {
             public void onErrorResponse(VolleyError error) {}
         });
         queue.add(validIDsRequest);
+    }
+
+    public void setSingleLayoutVisible(int visibleLayoutId) {
+        for (int i = 0 ; i < layouts.length ; i++) {
+            if (visibleLayoutId == layouts[i].getId()) {
+                layouts[i].setVisibility(View.VISIBLE);
+            }
+            else {
+                layouts[i].setVisibility(View.GONE);
+            }
+        }
     }
 }
